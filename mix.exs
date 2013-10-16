cc0e8d090c02801a5653c81a1c93702888634cff
Éverton Ribeiro & Renato <nuxlli@gmail.com>
2013-10-15 21:52:08 -0300

    Removing node and adding elixir (for winners)

---
+++ mix.exs
@@ -0,0 +1,21 @@
+defmodule Azk.Mixfile do
+  use Mix.Project
+
+  def project do
+    [ app: :azk,
+      version: "0.0.1",
+      elixir: "~> 0.10.3",
+      deps: deps ]
+  end
+
+  # Configuration for the OTP application
+  def application do
+    [mod: { Azk, [] }]
+  end
+
+  # Returns the list of dependencies in the format:
+  # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
+  defp deps do
+    []
+  end
+end
---
+++ test/azk_test.exs
@@ -0,0 +1,7 @@
+defmodule AzkTest do
+  use ExUnit.Case
+
+  test "the truth" do
+    assert(true)
+  end
+end
