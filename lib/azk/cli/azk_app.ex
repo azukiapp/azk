d26f322490b63f3159eb744ee700ce4d6f2001ca
Éverton Ribeiro <nuxlli@gmail.com>
2013-10-18 19:06:32 -0300

    Adding AzkApp

---
+++ lib/azk/cli/azk_app.ex
@@ -0,0 +1,52 @@
+defmodule Azk.Cli.AzkApp do
+  alias Azk.Cli.Utils
+  alias Azk.Config
+
+  # Fields
+  @fields [ id: nil, azkfile: nil, path: nil, box: nil,
+    options: [], envs: [], builds: [], services: []
+  ]
+
+  @type t :: __MODULE__.t
+
+  # Record def
+  Record.deffunctions(@fields, __ENV__)
+  Record.import __MODULE__, as: :app
+
+  defoverridable new: 1
+  def new(opts) do
+    opts = Keyword.merge([
+      path: System.cwd!,
+      azkfile: find_azkfile(opts[:path] || System.cwd!)
+    ], opts)
+    super opts
+  end
+
+  @doc """
+  Load app information from #{Azk.azkfile}
+  """
+  @spec load!(t) :: t
+  def load!(app(azkfile: azkfile, path: path)) do
+    case Utils.parse_azkfile(azkfile) do
+      {:ok, azkfile} ->
+        app(app, id: azkfile[:id])
+      _ ->
+        raise Azk.Cli.NoAzkfileError.new(app_folder: path)
+    end
+  end
+
+  @doc """
+  Return a path to mount app in azk folder
+  """
+  @spec mount_folder(t) :: String.t
+  def mount_folder(app(id: id)) do
+    Path.join([Config.get(:azk_agent_path), "apps", id])
+  end
+
+  defp find_azkfile(path) do
+    case Utils.find_azkfile(path) do
+      {:ok, file} -> file
+      {_, file, _} -> file
+    end
+  end
+end
---
+++ test/azk/cli/azk_app_test.exs
@@ -0,0 +1,51 @@
+defmodule Azk.Cli.AzkApp.Test do
+  use Azk.TestCase
+  alias Azk.Cli.AzkApp
+  alias Azk.Cli.Utils
+
+  test :is_record do
+    assert is_record(AzkApp.new, AzkApp)
+  end
+
+  test "set azkfile based in options" do
+    path = fixture_path(:full_azkfile)
+    app  = AzkApp.new(path: path)
+    assert path == app.path
+    assert Utils.find_azkfile!(path) == app.azkfile
+  end
+
+  test "capture the path from cwd" do
+    path = fixture_path(:full_azkfile)
+    File.cd! path, fn ->
+      app = AzkApp.new()
+      assert path == app.path
+      assert Utils.find_azkfile!(path) == app.azkfile
+    end
+  end
+
+  test "parse azkfile and set values" do
+    path = fixture_path(:full_azkfile)
+    File.cd! path, fn ->
+      app = AzkApp.new().load!
+      refute nil == app.id
+    end
+  end
+
+  test "raise exception azkfile not found" do
+    app_path = fixture_path(:no_azkfile)
+    File.cd! app_path, fn ->
+      assert_raise Azk.Cli.NoAzkfileError, %r/#{app_path}/, fn ->
+        AzkApp.new.load!
+      end
+    end
+  end
+
+  test "get azk folder path" do
+    path = fixture_path(:full_azkfile)
+    File.cd! path, fn ->
+      apps_folder = Path.join([Azk.Config.get(:azk_agent_path), "apps"])
+      app = AzkApp.new.load!
+      assert Path.join([apps_folder, app.id]) == app.mount_folder
+    end
+  end
+end
