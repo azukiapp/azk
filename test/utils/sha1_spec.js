1382f448b034284b88bec3eff44e376a01cafef9
Éverton Ribeiro <nuxlli@gmail.com>
2014-02-05 16:26:05 -0200

    Refactoring sha1, now using the more newest file to calculate hash.

--- test/utils/sha1_spec.js
+++ test/utils/sha1_spec.js
@@ -8,4 +8,9 @@ describe("Azk utils sha1", function() {
     var box_path = helper.fixture_path("test-box");
     return expect(sha1.calculate(box_path)).to.eventually.match(/^\w{40}$/)
   });
+
+  it("should rais exception if path not a directory", function() {
+    return expect(sha1.calculate(__filename))
+      .to.eventually.rejectedWith(Error, /Source .* not is a directory/);
+  });
 });
