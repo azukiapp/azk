4061173a71aecc2612c3e6e43b97fab3f00ac729
Éverton Ribeiro <nuxlli@gmail.com>
2014-02-11 22:11:28 -0200

    Refactoring exec command to use app image.

--- test/commands/exec_spec.js
+++ test/commands/exec_spec.js
@@ -3,12 +3,15 @@ var child = require('child_process');
 var MemoryStream = require('memorystream');
 var path  = require('path');
 var docker = require('../../lib/docker');
+var App   = require('../../lib/app');
 
 var azk = h.azk;
 var Q   = azk.Q;
 var _   = azk._;
 
-describe.only("Azk exec command", function() {
+describe("Azk exec command", function() {
+  var app_dir = h.fixture_path('test-app');
+  var app     = new App(app_dir);
   var stdout, stderr, outputs;
 
   beforeEach(function() {
@@ -23,9 +26,10 @@ describe.only("Azk exec command", function() {
     });
   });
 
-  function exec(args, stdin) {
+  function exec(dir, args, stdin) {
     var done = Q.defer();
     var opts = {
+      cwd: dir,
       env: _.extend({ DEBUG: "azk:*" }, process.env),
     }
 
@@ -49,46 +53,87 @@ describe.only("Azk exec command", function() {
     return done.promise;
   }
 
-  it("should execute a command and remove a container", function() {
-    var result = exec(["/bin/bash", "-c", "ls -l"])
-    .then(function(code) {
-      var stderr = outputs.stderr;
-      var regex  = /azk:exec created: (.*)/;
-      var id     = stderr.match(regex)[1];
+  it("should execute and return error if azkfile.json not found", function() {
+    return h.tmp.dir().then(function(tmp) {
+      var result = exec(tmp, ["/bin/true"]);
 
-      h.expect(stderr).to.match(regex);
-      h.expect(id).to.match(/[0-9a-f]{64}/);
+      return result.then(function(code) {
+        var msg = azk.t("app.box.not_found", azk.cst.MANIFEST);
 
-      return docker.getContainer(id).remove();
+        h.expect(code).to.equal(1);
+        h.expect(outputs.stderr).to.match(RegExp(msg));
+      });
     });
-
-    return h.expect(result)
-      .to.eventually.rejectedWith(/404/);
   });
 
-  it("should execute a command and not remove a container", function() {
-    var result = exec(["-R", "/bin/bash", "-c", "ls -l"])
-    .then(function(code) {
-      var stderr = outputs.stderr;
-      var regex  = /azk:exec created: (.*)/;
-      var id     = stderr.match(regex)[1];
+  it("should return error if not provision image", function() {
+    return h.remove_images(app.image).then(function() {
+      var result = exec(app.path, ["/bin/true"]);
 
-      h.expect(stderr).to.not.match(/removing container/);
-      h.expect(stderr).to.match(regex);
-      h.expect(id).to.match(/[0-9a-f]{64}/);
+      return result.then(function(code) {
+        var msg = azk.t("app.image.not_provision", app.image);
 
-      return docker.getContainer(id).remove();
+        h.expect(code).to.equal(2);
+        h.expect(outputs.stderr).to.match(RegExp(msg));
+      });
     });
-
-    return h.expect(result).to.fulfilled;
   });
 
-  it("should execute a interative command", function() {
-    var result = exec(["-i", "/bin/bash"], ["uptime", "exit"])
-    .then(function(code) {
-      h.expect(outputs.stdout).to.match(/.*users.*load average.*/);
+  describe("in a provisioned app", function() {
+    before(function() {
+      this.timeout(0);
+      return app.provision({ force: true }, new MemoryStream());
     });
 
-    return h.expect(result).to.fulfilled;
+    it("should execute a command and remove a container", function() {
+      var result = exec(app.path, ["/bin/bash", "-c", "ls -l"])
+      .then(function(code) {
+        var stderr = outputs.stderr;
+        var regex  = /azk:exec created: (.*)/;
+        var id     = stderr.match(regex)[1];
+
+        h.expect(stderr).to.match(regex);
+        h.expect(id).to.match(/[0-9a-f]{64}/);
+
+        return docker.getContainer(id).remove();
+      });
+
+      return h.expect(result)
+        .to.eventually.rejectedWith(/404/);
+    });
+
+    it("should execute a command and not remove a container", function() {
+      var cmd = ["-R", "/bin/bash", "-c", "ls -l"];
+
+      return exec(app.path, cmd).then(function(code) {
+        var stderr = outputs.stderr;
+        var regex  = /azk:exec created: (.*)/;
+        var id     = stderr.match(regex)[1];
+
+        h.expect(stderr).to.not.match(/removing container/);
+        h.expect(stderr).to.match(regex);
+        h.expect(id).to.match(/[0-9a-f]{64}/);
+
+        return docker.getContainer(id).remove();
+      });
+    });
+
+    it("should execute a interative command", function() {
+      var cmd = ["-i", "/bin/bash"];
+      var stdin = ["uptime", "exit"];
+
+      return exec(app.path, cmd, stdin).then(function(code) {
+        h.expect(outputs.stdout).to.match(/.*users.*load average.*/);
+      });
+    });
+
+    it("should mount app directory", function() {
+      var cmd = ["/bin/bash", "-c", "cat /azk/app/azkfile.json"];
+
+      return exec(app.path, cmd).then(function(code) {
+        h.expect(code).to.equal(0);
+        h.expect(outputs.stdout).to.match(RegExp(app.id));
+      });
+    });
   });
 });
