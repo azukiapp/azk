599b4a2bf4f5f363c672215c9e8fbdf93b224d17
Everton Ribeiro <nuxlli@gmail.com>
2014-04-02 20:44:54 -0300

    Initial manifest code and tests.

--- spec/manifest/index_spec.js
+++ spec/manifest/index_spec.js
@@ -1,8 +1,18 @@
-import { expect, fixture_path } from 'spec/spec_helper';
+import { config } from 'azk';
 import { Manifest } from 'azk/manifest';
+import h from 'spec/spec_helper';
 
 describe("Azk manifest module", function() {
-  it("should find manifest", function() {
+  it("should return not found manifest", function() {
+    return h.tmp_dir({ prefix: "azk-" }).then(function(dir) {
+      h.expect(Manifest.find_manifest(dir)).to.equal(null);
+    });
+  });
 
+  describe("dsl spec", function() {
+    it("should require and run a file", function() {
+      var project = h.fixture_path('full_example');
+      console.log(project)
+    });
   });
 });
--- src/manifest/index.js
+++ src/manifest/index.js
@@ -1,10 +1,12 @@
-import { cst } from 'azk';
+import { sync as parent } from 'parentpath';
+import { cst, config } from 'azk';
+import Utils from 'azk/utils';
 
 export class Manifest {
   static find_manifest(target) {
-    var dir = azk.utils.cd(target, function() {
-      return parent(azk.cst.MANIFEST);
+    var dir = Utils.cd(target, function() {
+      return parent(config('MANIFEST'));
     });
-    return dir ? path.join(dir, azk.cst.MANIFEST) : null;
+    return dir ? path.join(dir, config('MANIFEST')) : null;
   }
 }
