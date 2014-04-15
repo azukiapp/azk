3dafe22441f588553abcdba89e12a6582a99feae
Everton Ribeiro <nuxlli@gmail.com>
2014-04-15 17:13:31 -0300

    Adding helper defer in azk module.

--- src/index.js
+++ src/index.js
@@ -11,9 +11,18 @@ function pp(...args) {
   return console.log(...args);
 }
 
+function defer(func) {
+  var done   = Q.defer();
+  var result = func(done);
+  if (Q.isPromise(result)) {
+    result.progress(done.notify).then(done.resolve, done.reject);
+  }
+  return done.promise;
+}
+
 // Default i18n method
 var t = new i18n({ locale: config('locale') }).t;
 
-export { pp, t };
+export { pp, t, defer };
 export { Q, _ as unders, _, config };
 export default Azk;
