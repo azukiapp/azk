e438240a6434a73396453496012437fb8a2a57ed
Everton Ribeiro <nuxlli@gmail.com>
2014-04-15 17:14:15 -0300

    Refactoring dustman helper to split remove imagens and containers.

--- spec/spec_helpers/dustman.js
+++ spec/spec_helpers/dustman.js
@@ -1,5 +1,5 @@
 import docker from 'azk/docker';
-import { Q, Azk, pp, _, config, t } from 'azk';
+import { Q, Azk, pp, _, config, t, defer } from 'azk';
 
 export function extend(Helpers) {
   // Filters
@@ -16,33 +16,42 @@ export function extend(Helpers) {
     return _.some(t_regexs, (regex) => { return tag.match(regex) });
   }
 
+  Helpers.remove_containers = function() {
+    return defer((done) => {
+      return docker.listContainers({ all: true }).then((containers) => {
+        containers = _.filter(containers, filter_containers);
+        done.notify(t('test.remove_containers', containers.length));
+        return Q.all(_.map(containers, (container) => {
+          return docker.getContainer(container.Id).remove({ force: true });
+        }));
+      });
+    });
+  }
+
+  Helpers.remove_images = function() {
+    return defer((done) => {
+      return docker.listImages().then((images) => {
+        var tags = _.flatten(_.map(
+          images, (image) => { return image.RepoTags }
+        ));
+
+        tags = _.filter(tags, filter_tags);
+        done.notify(t('test.remove_images', tags.length));
+
+        return Q.all(_.map(tags, (tag) => {
+          return docker.getImage(tag).remove();
+        }));
+      });
+    });
+  }
+
   // Remove all containers before run
   before(function() {
     this.timeout(0);
     console.log(t('test.before'));
-
-    var results = Q.async(function* () {
-      // Remove containers
-      var containers = yield docker.listContainers({ all: true });
-      containers = _.filter(containers, filter_containers);
-      console.log(t('test.remove_containers', containers.length));
-      yield Q.all(_.map(containers, (container) => {
-        return docker.getContainer(container.Id).remove({ force: true });
-      }));
-
-      // Remove images
-      var tags   = _.flatten(_.map(
-        yield docker.listImages(),
-        (image) => { return image.RepoTags }
-      ));
-
-      tags = _.filter(tags, filter_tags);
-      console.log(t('test.remove_images', tags.length));
-      yield Q.all(_.map(tags, (tag) => {
-        return docker.getImage(tag).remove();
-      }));
-    })();
-
-    return results.then(() => console.log("\n"));
+    return Helpers.remove_containers()
+      .then(Helpers.remove_images)
+      .then(() => console.log("\n"))
+      .progress((event) => console.log(`  ${event}`));
   });
 }
