511b057457945c116bf989ae537f96848103a4b4
Everton Ribeiro <nuxlli@gmail.com>
2014-07-21 18:22:32 -0300

    Adding initial code to scale system and testes. #49

--- spec/system/scale_spec.js
+++ spec/system/scale_spec.js
@@ -1,8 +1,9 @@
 import h from 'spec/spec_helper';
-import { config } from 'azk';
+import { config, async, Q } from 'azk';
 import { System } from 'azk/system';
 import { Scale } from 'azk/system/scale';
 import { SystemDependError } from 'azk/utils/errors';
+import docker from 'azk/docker';
 
 describe("systems, scale", function() {
   var manifest, system;
@@ -15,9 +16,48 @@ describe("systems, scale", function() {
   });
 
   describe("scale one system", function() {
+    afterEach(function() {
+      return system.killAll().fail(() => {});
+    });
+
     it("should not run system if its dependencies are not met", function() {
-      var result = Scale.scale(system, 1);
-      return h.expect(result).to.eventually.rejectedWith(SystemDependError)
+      var result = system.scale(1);
+      return h.expect(result).to.eventually.rejectedWith(SystemDependError);
+    });
+
+    it("should scale one instances", function() {
+      var db = manifest.system('db');
+      return async(this, function* () {
+        var result = yield db.scale(1);
+        var instances = yield db.instances();
+
+        h.expect(result).to.ok;
+        h.expect(instances).to.length(1);
+
+        var container   = yield docker.getContainer(instances[0].Id).inspect();
+        var annotations = container.Annotations.azk;
+        h.expect(annotations).to.have.deep.property("type", 'daemon');
+        h.expect(annotations).to.have.deep.property("sys", db.name);
+        h.expect(annotations).to.have.deep.property("seq", '1');
+      });
+    });
+
+    it("should scale a system with dependencies", function() {
+      return async(this, function* () {
+        yield manifest.system('db').scale(1);
+        yield manifest.system('api').scale(1);
+        var result = yield system.scale(3);
+        var instances = yield system.instances();
+
+        h.expect(result).to.ok;
+        h.expect(instances).to.length(3);
+
+        var container   = yield docker.getContainer(instances[0].Id).inspect();
+        var annotations = container.Annotations.azk;
+        h.expect(annotations).to.have.deep.property("type", 'daemon');
+        h.expect(annotations).to.have.deep.property("sys", system.name);
+        h.expect(annotations).to.have.deep.property("seq", '1');
+      });
     });
   });
 });
--- src/system/scale.js
+++ src/system/scale.js
@@ -3,15 +3,41 @@ import { SystemDependError } from 'azk/utils/errors';
 import docker from 'azk/docker';
 
 var Scale = {
-
   scale(system, instances, options = {}) {
     // Default options
     options = _.defaults(options, {
-    })
+    });
 
-    return async(this, function* () {
+    return async(this, function* (notify) {
       yield this.checkDepends(system);
+      var containers = yield this.instances(system);
+
+      var from = containers.length;
+      var icc  = instances - from;
+
+      if (icc != 0)
+        notify({ type: "scale", from, to: from + icc, system: this.name });
+
+      if (icc > 0) {
+        for(var i = 0; i < icc; i++) {
+          yield system.runDaemon();
+        }
+      } else if (icc < 0) {
+        containers = containers.reverse().slice(0, Math.abs(icc));
+        yield this._kill_or_stop(containers);
+      }
+
       return true;
+    });
+  },
+
+  killAll(system, options = {}) {
+    options = _.defaults(options, {
+      kill: true,
+    });
+
+    return this.instances(system).then((instances) => {
+      return system.stop(instances, options.kill);
     });
   },
 
@@ -22,19 +48,30 @@ var Scale = {
       for (var d = 0; d < depends.length; d++) {
         depend    = depends[d];
         instances = yield this.instances(depend);
-        if (instances) {
+        if (_.isEmpty(instances)) {
           throw new SystemDependError(system.name, depend.name);
         }
       }
-
       return true;
     });
   },
 
-  instances(system, include_dead = false) {
-    if (include_dead) include_dead = { all: true };
-    return docker.listContainers(include_dead).then((containers) => {
-      return system.filter(containers);
+  instances(system, options = {}) {
+    // Default options
+    options = _.defaults(options, {
+      include_dead: false,
+      include_exec: false,
+    })
+
+    // Include dead containers
+    var query_options = {};
+    if (options.include_dead) query_options.all = true ;
+
+    return docker.azkListContainers(query_options).then((containers) => {
+      return _.filter(containers, (container) => {
+        var azk = container.Annotations.azk;
+        return azk.mid == system.manifest.namespace && azk.sys == system.name;
+      });
     });
   },
 }
