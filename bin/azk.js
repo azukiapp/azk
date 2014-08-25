52bea49196fe55c189d9d0598245f567ea20ace5
Everton Ribeiro <nuxlli@gmail.com>
2014-08-24 23:10:40 -0300

    Replacing bash script to node vm and fix ps name. #77

--- bin/azk.js
+++ bin/azk.js
@@ -7,4 +7,10 @@ process.once("azk:command:exit", function(code) {
   process.exit(code);
 });
 
+// ps name
+var path = require('path');
+title = path.basename(__filename, ".js");
+process.title = [title].concat(process.argv.slice(2)).join(" ");
+
+// Run cli
 cli(process.argv, process.env.AZK_CURRENT_SYSTEM || process.cwd());
