efebe2a9e1c28fdeb9eecbcc624d5d8aa9ba7189
Julio Makdisse Saito <saitodisse@gmail.com>
2015-01-06 17:49:36 -0200

    [204_image_azkfile] Creating better comments to the code

--- src/images/index.js
+++ src/images/index.js
@@ -16,7 +16,7 @@ lazy_require(this, {
 export class Image {
   constructor(image) {
 
-    // only a string, will be deprecated
+    // 1. only a string, will be deprecated. i.e.: 'azukiapp/azktcl:0.0.2'
     if (_.isString(image)) {
       this.provider = 'docker';
       this.isDeprecated = true;
@@ -24,25 +24,19 @@ export class Image {
       return null;
     }
 
+    // 2. provider object. i.e.: { docker: 'azukiapp/azktcl:0.0.2' }
     var keys = _.keys(image);
-    if (keys.length === 1) {
+    if (keys.length === 1 && (keys[0] === 'docker' ||
+                              keys[0] === 'dockerfile') ) {
       this.provider = keys[0];
-
-      if (this.provider === 'docker') {
-        this.name = image[keys[0]];
-        return null;
-      } else{
-        this.repository = image.repository;
-        this.tag        = image.tag      || default_tag;
-        this.provider   = image.provider || default_provider;
-      }
-    }
-    else {
-      this.repository = image.repository;
-      this.tag        = image.tag      || default_tag;
-      this.provider   = image.provider || default_provider;
+      this.name = image[keys[0]];
+      return null;
     }
 
+    // 3. specific properties
+    this.repository = image.repository;
+    this.tag        = image.tag      || default_tag;
+    this.provider   = image.provider || default_provider;
   }
 
   check() {
