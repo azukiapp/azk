26683c453919f0aeef93ad362575e65486aa561b
Julio Makdisse Saito <saitodisse@gmail.com>
2015-01-27 16:36:15 -0200

    [jshint jscs grunt] Including jshint and jscs to grunt hint task #129

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,21 @@
 'use strict';
 
-function key_watch(grunt) {
+function key_watch() {
   var keypress = require('keypress');
-  var stdin = process.openStdin();
   var touch = require('touch');
 
-  // make `process.stdin` begin emitting "keypress" events
+  // make `process.stdin` begin emitting 'keypress' events
   keypress(process.stdin);
 
-  // listen for the "keypress" event
+  // listen for the 'keypress' event
   process.stdin.on('keypress', function (ch, key) {
     if (key) {
       if (key.ctrl && key.name == 'c') {
         process.stdin.pause();
         return process.exit();
-      } else if(key.name == "c") {
+      } else if (key.name == 'c') {
         process.stdout.write('\u001B[2J\u001B[0;0f');
-      } else if(key.name == "r") {
+      } else if (key.name == 'r') {
         // Reload
         touch(__filename);
       }
@@ -29,31 +28,31 @@ function key_watch(grunt) {
 
 module.exports = function(grunt) {
   // Test options
-  var test_task = grunt.option('test') || "test";
+  var test_task = grunt.option('test') || 'test';
   var test_grep = grunt.option('grep') || null;
-  if (test_grep != null) {
-    test_task = "slow_test";
+  if (test_grep !== null) {
+    test_task = 'slow_test';
   }
 
   // Lib path
-  var lib = process.env.AZK_LIB_PATH || "lib";
+  var lib = process.env.AZK_LIB_PATH || 'lib';
 
   // Project configuration.
   grunt.initConfig({
     // ENV's
     env: {
       test: {
-        NODE_ENV: "test",
+        NODE_ENV: 'test',
       },
       aws: {
-        src: ".env",
+        src: '.env',
       }
     },
 
     aws: {
-      "accessKeyId" : process.env.AWS_ACCESS_KEY_ID,
-      "secretKey"   : process.env.AWS_SECRET_KEY,
-      "bucket"      : process.env.AWS_BUCKET,
+      'accessKeyId' : process.env.AWS_ACCESS_KEY_ID,
+      'secretKey'   : process.env.AWS_SECRET_KEY,
+      'bucket'      : process.env.AWS_BUCKET,
     },
 
     aws_s3: {
@@ -69,10 +68,10 @@ module.exports = function(grunt) {
       },
       publish_package: {
         files: [
-          {expand: true, cwd: "./package/brew", src: ['*.tar.gz'], dest: "./mac/", stream: true },
-          {expand: true, cwd: "./package/aptly/public", src: ['**/*'], stream: true },
-          {expand: true, cwd: "./package/fedora20", src: ['**/*'], dest: "./fedora20/", stream: true },
-          //{expand: true, cwd: "./src/libexec/gpg", src: ['azuki.asc'], dest: "./keys/", stream: true },
+          {expand: true, cwd: './package/brew', src: ['*.tar.gz'], dest: './mac/', stream: true },
+          {expand: true, cwd: './package/aptly/public', src: ['**/*'], stream: true },
+          {expand: true, cwd: './package/fedora20', src: ['**/*'], dest: './fedora20/', stream: true },
+          //{expand: true, cwd: './src/libexec/gpg', src: ['azuki.asc'], dest: './keys/', stream: true },
         ],
       },
     },
@@ -80,7 +79,7 @@ module.exports = function(grunt) {
     // Downloads
     'curl-dir': {
       'brace-expansion': {
-        src: [ "https://s3-sa-east-1.amazonaws.com/azk/azk{.iso,-agent.vmdk.gz}" ],
+        src: [ 'https://s3-sa-east-1.amazonaws.com/azk/azk{.iso,-agent.vmdk.gz}' ],
         dest: lib + '/vm',
       },
     },
@@ -120,7 +119,7 @@ module.exports = function(grunt) {
         options: {
           reporter: 'spec',
           timeout: 5000,
-          grep: "@slow",
+          grep: '@slow',
           invert: true,
         },
         src: [lib + '/spec/**/*_spec.js']
@@ -165,13 +164,38 @@ module.exports = function(grunt) {
     exec: {
       'build': {
         'cmd': function(system) {
-          return 'azk shell ' + system + ' --shell=/bin/bash -c "azk nvm grunt newer:traceur"';
+          return 'azk shell ' +
+            system +
+            ' --shell=/bin/bash -c "azk nvm grunt newer:traceur"';
         },
       },
       'publish_package': {
-        'cmd': "grunt aws_s3:publish_package"
+        'cmd': 'grunt aws_s3:publish_package'
       }
     },
+
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'src/**/*.js',
+        'spec/**/*.js',
+      ],
+      options: {
+        jshintrc: true
+      }
+    },
+
+    jscs: {
+      all: [
+        'Gruntfile.js',
+        'src/**/*.js',
+        'spec/**/*.js',
+      ],
+      options: {
+        config: '.jscsrc'
+      }
+    },
+
   });
 
   // load all grunt tasks matching the `grunt-*` pattern
@@ -182,12 +206,13 @@ module.exports = function(grunt) {
     process.stdout.write('\u001B[2J\u001B[0;0f');
   });
 
-  grunt.registerTask('vm-download', [ 'curl-dir:brace-expansion' ]);
-  grunt.registerTask('test'       , ['env:test', 'clear', 'newer:traceur', 'mochaTest:test']);
-  grunt.registerTask('slow_test'  , ['env:test', 'clear', 'newer:traceur', 'mochaTest:slow_test']);
+  grunt.registerTask('vm-download', ['curl-dir:brace-expansion' ]);
+  grunt.registerTask('test'       , ['env:test', 'clear', 'newer:traceur', 'hint', 'mochaTest:test']);
+  grunt.registerTask('slow_test'  , ['env:test', 'clear', 'newer:traceur', 'hint', 'mochaTest:slow_test']);
   grunt.registerTask('compile'    , ['watch:traceur']);
-  grunt.registerTask('inspector'  , ["node-inspector"]);
-  grunt.registerTask('publish'    , ["env:aws", "exec:publish_package"]);
+  grunt.registerTask('inspector'  , ['node-inspector']);
+  grunt.registerTask('hint'       , ['jshint', 'jscs']);
+  grunt.registerTask('publish'    , ['env:aws', 'exec:publish_package']);
   grunt.registerTask('default'    , function() {
     key_watch(grunt);
     return grunt.task.run(['watch:spec']);
