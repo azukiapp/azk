c06be7429ae37eeb204ef4aedf88ea2c449062b8
Julio Makdisse Saito <saitodisse@gmail.com>
2015-03-11 00:09:08 -0300

    [pull progress bar] Creating DownloadPart class

--- spec/cli/smart_progress_bar_spec.js
+++ spec/cli/smart_progress_bar_spec.js
@@ -7,32 +7,63 @@ describe('SmartProgressBar', function() {
   var smartProgressBar;
 
   beforeEach(function () {
-    smartProgressBar = new SmartProgressBar();
+    smartProgressBar = new SmartProgressBar(50, 10);
   });
 
   it('should SmartProgressBar be instantiated', function() {
     h.expect(smartProgressBar).to.not.be.undefined;
   });
 
-  it('should has parts', function() {
+  it('should have parts', function() {
     h.expect(_.isArray(smartProgressBar._download_parts)).to.be.true;
+  });
+
+  it('should have bars count', function() {
+    h.expect(_.isNumber(smartProgressBar._bar_count)).to.be.true;
+  });
+
+  it('should have layers count', function() {
+    h.expect(_.isNumber(smartProgressBar._layers_count)).to.be.true;
+  });
+
+  it('should calculate bars per layers', function() {
+    h.expect(smartProgressBar._bars_per_layers).to.be.equal(5);
+  });
+
+  it('should calculate percentage tick', function() {
+    h.expect(smartProgressBar._percentage_tick).to.be.equal(0.2);
   });
 
   it('should add and get a part', function() {
     var msg = {
-      id            : 'abcd',
+      id: 'abcd',
       progressDetail: {
         current: 200,
         total  : 2000
       }
     };
 
-    smartProgressBar.addPart(msg);
+    smartProgressBar.receiveMessage(msg);
 
-    var part1 = smartProgressBar.getPartById('abcd');
+    var part1 = smartProgressBar.getPart(msg);
     h.expect(part1.id).to.be.equal('abcd');
     h.expect(part1.current_downloaded_size).to.be.equal(200);
     h.expect(part1.total_downloaded_size).to.be.equal(2000);
+  });
+
+  it('should calculate percetage', function() {
+    var msg = {
+      id: 'abcd',
+      progressDetail: {
+        current: 200,
+        total  : 2000
+      }
+    };
+
+    smartProgressBar.receiveMessage(msg);
+
+    var download_part1 = smartProgressBar.getPart(msg);
+    h.expect(download_part1.getTotalPercentage()).to.be.equal(0.10);
   });
 
   it('should calculate percetage', function() {
@@ -44,10 +75,32 @@ describe('SmartProgressBar', function() {
       }
     };
 
-    smartProgressBar.addPart(msg);
+    smartProgressBar.receiveMessage(msg);
 
-    var download_part1 = smartProgressBar.getPartById('abcd');
+    var download_part1 = smartProgressBar.getPart(msg);
     h.expect(download_part1.getTotalPercentage()).to.be.equal(0.10);
   });
+
+  it('should update downloaded_part when receives same message', function() {
+    var msg = { id: 'abcd', progressDetail: { current: 200, total  : 2000 } };
+    smartProgressBar.receiveMessage(msg);
+    h.expect(smartProgressBar.getPart(msg).current_downloaded_size)
+      .to.be.equal(200);
+
+    msg = { id: 'abcd', progressDetail: { current: 300, total  : 2000 } };
+    smartProgressBar.receiveMessage(msg);
+    h.expect(smartProgressBar.getPart(msg).current_downloaded_size)
+      .to.be.equal(300);
+
+  });
+
+  // it('should emit a tick when _percentage_tick is reach', function() {
+  //   var msg = { id: 'abcd', progressDetail: { current: 200, total  : 2000 } };
+
+  //   smartProgressBar.receiveMessage(msg);
+
+  //   var download_part1 = smartProgressBar.getPart(msg);
+  //   h.expect(download_part1.getTotalPercentage()).to.be.equal(0.10);
+  // });
 
 });
--- src/cli/smart_progress_bar.js
+++ src/cli/smart_progress_bar.js
@@ -1,35 +1,53 @@
 import { _ } from 'azk';
-
 // var ProgressBar = require('progress');
 
 class DownloadPart {
-  constructor(id, current_downloaded_size, total_downloaded_size) {
-    this.id = id;
-    this.current_downloaded_size = current_downloaded_size;
-    this.total_downloaded_size = total_downloaded_size;
+  constructor(msg) {
+    this.id = msg.id;
+    this.current_downloaded_size = msg.progressDetail.current;
+    this.total_downloaded_size = msg.progressDetail.total;
   }
 
   getTotalPercentage() {
     return this.current_downloaded_size / this.total_downloaded_size;
   }
+
+  update(msg) {
+    this.current_downloaded_size = msg.progressDetail.current;
+  }
 }
 
-// TODO: Implement tests
 export class SmartProgressBar {
 
-  constructor() {
+  constructor(bar_count, layers_count) {
     this._download_parts = [];
+    this._bar_count = bar_count;
+    this._layers_count = layers_count;
+    this._bars_per_layers = this._calculate_bars_per_layers();
+    this._percentage_tick = this._calculate_percentage_tick();
   }
 
-  addPart(msg) {
-    var downloadPart = new DownloadPart(msg.id, msg.progressDetail.current, msg.progressDetail.total);
-
-    this._download_parts.push(downloadPart);
+  _calculate_bars_per_layers() {
+    return this._bar_count / this._layers_count;
   }
 
-  getPartById(id) {
+  _calculate_percentage_tick() {
+    return 1.0 / this._bars_per_layers;
+  }
+
+  receiveMessage(msg) {
+    var current_download_part = this.getPart(msg);
+    if (!current_download_part) {
+      var downloadPart = new DownloadPart(msg);
+      this._download_parts.push(downloadPart);
+    } else {
+      current_download_part.update(msg);
+    }
+  }
+
+  getPart(msg) {
     return _.find(this._download_parts, function(part) {
-      return part.id === id;
+      return part.id === msg.id;
     });
   }
 
