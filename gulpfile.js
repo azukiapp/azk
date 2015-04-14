da550d1b6a209954ac94d38e2787f9225590eefa
Éverton Ribeiro <nuxlli@gmail.com>
2015-04-14 16:46:30 -0300

    Moving vm-download from grunt to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,18 @@ var azk_gulp = require('azk-dev/gulp')({
   lint: [ "bin/**/*.js" ],
 });
 
-// var gulp = azk_gulp.gulp;
+// Load gulp
+var gulp = azk_gulp.gulp;
+
+gulp.task("vm-download", "Download vm files: iso and data disk exemple", function() {
+  var url     = 'https://s3-sa-east-1.amazonaws.com/azk/debian2docker/';
+  var files   = ['azk.iso', 'azk-agent.vmdk.gz'];
+  var download = require("gulp-download");
+  for (var i = 0; i < files.length; i++) {
+    download(url + files[i])
+      .pipe(gulp.dest('./lib/vm'));
+  }
+});
 
 // gulp.task("show:args", "Help text", ["before:show"], function() {
 //   console.log(azk_gulp.yargs.argv);
