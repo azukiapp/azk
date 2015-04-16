2101befe029c58e2589b20e3fd49e6ae0174eef7
Julio Makdisse Saito <saitodisse@gmail.com>
2015-04-16 17:33:53 -0300

    [tracking] Including envs to change keens API keys

--- src/config.js
+++ src/config.js
@@ -132,7 +132,11 @@ var options = mergeConfig({
   },
   test: {
     paths: {
+      // jscs:disable maximumLineLength
       log : path.join(paths.logs, 'azk_test.log'),
+      projectId: envs('AZK_KEEN_PROJECT_ID', '5526968d672e6c5a0d0ebec6'),
+      writeKey:  envs('AZK_KEEN_WRITE_KEY', '5dbce13e376070e36eec0c7dd1e7f42e49f39b4db041f208054617863832309c14a797409e12d976630c3a4b479004f26b362506e82a46dd54df0c977a7378da280c05ae733c97abb445f58abb56ae15f561ac9ad774cea12c3ad8628d896c39f6e702f6b035541fc1a562997cb05768'),
+      // jscs:enabled maximumLineLength
     },
     docker: {
       namespace   : 'azk.test',
