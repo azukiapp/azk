a9c709b49161fa5ff5f9819b157b9bb985b89cb4
Éverton Ribeiro <nuxlli@gmail.com>
2015-04-16 16:40:43 -0300

    [babel] Moving publish task to gulp and finally removing grunt

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,10 @@ var azk_gulp = require('azk-dev/gulp')({
 // Load gulp
 var gulp = azk_gulp.gulp;
 
+// Load envs from .env files
+var dotenv = require('dotenv');
+dotenv.load();
+
 var integration_src = [
   'src/**/*.js',
   'spec/**/*.js',
@@ -39,3 +43,49 @@ gulp.task('test:integration', "Run integration tests", ['lint:babel'], function(
 });
 
 azk_gulp.new_watch('watch:test:integration', integration_src, {}, ['test:integration']);
+
+// Publish packages
+gulp.task('publish', function() {
+  var awspublish  = require('gulp-awspublish');
+  var parallelize = require("concurrent-transform");
+  var rename      = require("gulp-rename");
+  var gulpif      = require("gulp-if");
+  var path_join   = require('path').join;
+
+  // create a new publisher
+  var publisher = awspublish.create({
+    key: process.env.AWS_ACCESS_KEY_ID,
+    secret: process.env.AWS_SECRET_KEY,
+    bucket: process.env.AWS_PACKAGE_BUCKET,
+    region: 'sa-east-1',
+  });
+
+  // define custom headers
+  var headers = {
+    'Cache-Control': 'max-age=31536000, no-transform, public',
+  };
+
+  var src = [
+    'package/brew/*.tar.gz',
+    'package/public/!(fedora20)/**/*',
+    'package/fedora20/**/*',
+  ]
+
+  var add_prefix = function(cond, prefix) {
+    return gulpif(cond, rename(function (path) {
+        path.dirname = path_join(prefix, path.dirname);
+    }));
+  };
+
+  return gulp.src(src)
+    // Set dest path
+    .pipe(add_prefix(/package\/fedora20/, 'fedora20'))
+    .pipe(add_prefix(/package\/brew/, 'mac'))
+
+    // publisher will add Content-Length, Content-Type and headers specified above
+    // If not specified it will set x-amz-acl to public-read by default
+    .pipe(parallelize(publisher.publish(headers), 10))
+
+    // print upload updates to console
+    .pipe(awspublish.reporter());
+});
