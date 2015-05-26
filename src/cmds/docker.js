b8132d970445e71350070af7c6f692dc77c0f2cd
Gullit Miranda <gullitmiranda@gmail.com>
2015-05-26 03:30:51 -0300

    [Cli] Fixing `docker` and `adocker` command not working

--- src/cmds/docker.js
+++ src/cmds/docker.js
@@ -20,16 +20,12 @@ class Docker extends CliTrackerController {
         var point = config('agent:vm:mount_point');
         _path = utils.docker.resolvePath(this.cwd || '', point);
         args  = _.map(args, (arg) => arg.match(/^.* .*$/) ? `\\"${arg}\\"` : arg);
-        cmd   = `azk vm ssh -t 'cd ${_path}; docker ${args.join(" ")}'`;
+        cmd   = `azk vm ssh -- 'cd ${_path}; docker ${args.join(" ")}'`;
       }
 
       log.debug("docker options: %s", cmd);
       return this.ui.execSh(cmd);
     });
-  }
-
-  action(opts) {
-    return this.run_docker(opts);
   }
 }
 
