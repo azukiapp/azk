c196f9b2f8b0266d354455c8dc2c7fa7d674071f
Heitor Tashiro Sergent <heitortsergent@gmail.com>
2015-05-27 12:32:07 -0300

    [improving_suggestions] Adding PORT env variable setup for Node suggestions

--- src/generator/suggestions/node_default.js
+++ src/generator/suggestions/node_default.js
@@ -20,8 +20,20 @@ export class Suggestion extends UIProxy {
         '/azk/#{manifest.dir}': {type: 'path', value: '.'},
         '/azk/#{manifest.dir}/node_modules': {type: 'persistent', value: 'node-modules-#{system.name}'},
       },
+      envs_comment: [
+        'Make sure that the PORT value is the same as the one',
+        'in ports/http below, and that it\'s also the same',
+        'if you\'re setting it in a .env file'
+      ],
       envs    : {
-        NODE_ENV: "dev"
+        NODE_ENV: "dev",
+        // Make sure that the PORT value is the same as the one
+        // in ports/http below, and that it's also the same
+        // if you're setting it in a .env file
+        PORT: 3000
+      },
+      ports: {
+        http: '3000/tcp'
       }
     });
   }
