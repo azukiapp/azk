b110e1278bd27bd2ffb2b8d865f3b8a6c430ac8b
Felipe Arenales <fearenales@gmail.com>
2015-06-13 00:18:38 -0300

    [adding_tests_to_packaging_script] Generating multiple channels formulas for homebrew

--- src/libexec/package-tools/mac/generate.sh
+++ src/libexec/package-tools/mac/generate.sh
@@ -6,16 +6,29 @@ export VERSION=$( azk version | awk '{ print $2 }' )
 
 SHA256=$(shasum -a 256 shasum -a 256 "package/brew/azk_${VERSION}.tar.gz" | awk '{print $1}')
 
+RELEASE_CHANNEL=$( echo "${VERSION}" | sed s/[^\\-]*// | sed s/^\\-// | sed s/\\..*// )
+CLASS_NAME="Azk${RELEASE_CHANNEL^}"
+if [[ -z $RELEASE_CHANNEL ]]; then
+  CHANNEL_SUFFIX=
+  CONFLICTS=
+else
+  CHANNEL_SUFFIX="-${RELEASE_CHANNEL}"
+  CONFLICTS="
+  conflicts_with 'azk', :because => 'installation of azk in path'
+  "
+fi
+REPO_URL="repo${CHANNEL_SUFFIX}.azukiapp.com"
+
 rm -Rf /usr/local/Library/Taps/azukiapp
 git clone https://github.com/azukiapp/homebrew-azk /usr/local/Library/Taps/azukiapp/homebrew-azk
-tee /usr/local/Library/Taps/azukiapp/homebrew-azk/Formula/azk.rb <<EOF
+tee /usr/local/Library/Taps/azukiapp/homebrew-azk/Formula/azk${CHANNEL_SUFFIX}.rb <<EOF
 require "formula"
 
-class Azk < Formula
+class ${CLASS_NAME} < Formula
   homepage "http://azk.io"
-  url "http://repo.azukiapp.com/mac/azk_${VERSION}.tar.gz"
+  url "http://${REPO_URL}/mac/azk_${VERSION}.tar.gz"
   sha256 "${SHA256}"
-
+  ${CONFLICTS}
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
 
