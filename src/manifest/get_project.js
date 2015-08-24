c2c9c65184060d8abc1b51132bc2010cb4ae8894
Julio Makdisse Saito <saitodisse@gmail.com>
2015-08-24 12:48:56 -0300

    [azk start github again] Changing TODO comment

--- src/manifest/get_project.js
+++ src/manifest/get_project.js
@@ -108,13 +108,13 @@ export class GetProject extends UIProxy {
 
       // if exists, do a git pull inside
       if (dest_exists) {
-        // TODO: show a message
         this.warning('commands.start.get_project.dest_exists', {
           git_url              : command_parse_result.git_url,
           git_branch_tag_commit: command_parse_result.git_branch_tag_commit,
           git_destination_path : command_parse_result.git_destination_path,
         });
 
+        // TODO: show menu options
         // yield this._pullDestination(
         //   command_parse_result.git_url,
         //   command_parse_result.git_branch_tag_commit,
