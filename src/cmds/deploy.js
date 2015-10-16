a622addd58f80bbddced4cd496a3b3278fea1b95
Éverton Ribeiro <nuxlli@gmail.com>
2015-10-15 22:04:43 -0300

    [cli] Refactoring `azk deploy` to support `-c` and `--` in `ssh` and `shell`

--- src/cmds/deploy.js
+++ src/cmds/deploy.js
@@ -3,6 +3,7 @@ import { lazy_require, _ } from 'azk';
 import { async } from 'azk/utils/promises';
 import { AzkError } from 'azk/utils/errors';
 import { Helpers } from 'azk/cli/helpers';
+import { run as cli_run } from 'azk/cli';
 
 var lazy = lazy_require({
   Manifest: ['azk/manifest']
@@ -13,23 +14,40 @@ class Deploy extends CliTrackerController {
     return async(this, function* () {
       yield Helpers.requireAgent(this.ui);
 
-      var system = this._getDeploySystem(this.cwd);
-      var suffix = this.args.slice(1);
-      var cmd = [`azk shell ${system.name}`];
-      if (suffix) {
-        if (_.contains(suffix, "shell") || _.contains(suffix, "ssh")) {
-          if (_.contains(suffix, "--"))
-            suffix = _.filter(suffix, (s) => s != '--')
-          else {
-            cmd.push("--tty");
-          }
-        }
-        cmd.push('--');
-        cmd.push(suffix.join(' '));
-      }
-      cmd = cmd.join(" ");
+      // Get deploy [commands] avaible
+      var output  = [];
+      var mock_ui = { output: (line) => output.push(line) };
+      cli_run(["deploy", "--help"], this.cwd, mock_ui);
+      var args = output.join("").match(/Actions:([^]*)Arguments/)[0]
+      args = _.map(args.match(/^\s{2}([\w|-]*)/mg), (arg) => arg.trim());
 
-      return this._run(cmd);
+      var params   = this.normalized_params;
+      var system   = this._getDeploySystem(this.cwd);
+      var command  = _.find(args, (cmd) => params.commands[cmd]);
+      var cmd_head = ["shell", system.name];
+      var cmd_tail = [];
+
+      // Rollback to version
+      if (!_.isEmpty(params.arguments.ref)) {
+        cmd_tail.push(params.arguments.ref);
+      }
+
+      // Forwarding shell and ssh commands
+      if (params.commands.shell || params.commands.ssh) {
+        if (!_.isEmpty(params.options.command)) {
+          cmd_tail.push("-c", this._escape_quotes(params.options.command));
+        } else if (!_.isEmpty(params.arguments.args)) {
+          var escape_args = _.map(params.arguments.args, (arg) => this._escape_quotes(arg));
+          cmd_tail.push(...escape_args);
+        } else {
+          cmd_head.push("--tty");
+        }
+      }
+
+      var cmd = cmd_head.concat(["--", command, ...cmd_tail]);
+      var result;
+      [, result] = cli_run(cmd, this.cwd, this.ui);
+      return result;
     })
     .catch((err) => {
       if (err instanceof AzkError) {
@@ -39,6 +57,10 @@ class Deploy extends CliTrackerController {
       }
       return 1;
     });
+  }
+
+  _escape_quotes(cmd) {
+    return cmd.replace(/"/g, `\\"`);
   }
 
   _run(cmd) {
