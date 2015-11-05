a810d5a83ce0a295397b340e82c976ae0ad377a0
Éverton Ribeiro <nuxlli@gmail.com>
2015-11-05 04:37:30 -0200

    [Code] Refactoring Azkfile.js to simplify

--- Azkfile.js
+++ Azkfile.js
@@ -2,68 +2,72 @@
  * Documentation: http://docs.azk.io/Azkfile.js
  */
 
-var lodash = require('lodash');
-var join   = require('path').join;
-var config = require('azk').config;
+var mounts = {
+  "/azk/#{manifest.dir}": sync('./', { shell: true }),
+  "/azk/#{manifest.dir}/package": path("./package"),
+  "/azk/#{manifest.dir}/node_modules": persistent('node_modules-#{system.name}'),
+  "/azk/demos"          : path("../demos"),
+  "/azk/build"          : persistent('build-#{system.name}'),
+  "/azk/lib"            : persistent('lib-#{system.name}'),
+  "/azk/data"           : persistent('data-#{system.name}'),
+  "/azk/aptly"          : persistent('aptly-#{system.name}'),
+  "/var/lib/docker"     : persistent('docker_files-#{system.name}'),
+  "/root/.npm"          : persistent('npm-cache'),
+  "/usr/local/bin/make" : path("./src/libexec/make"),
+  "/root/.aptly.conf"   : path("./src/libexec/aptly.json"),
+  "/.tmux.conf"         : path(env.HOME + "/.tmux.conf"),
+};
 
-var mounts = (function() {
-  // Default mounts
-  var mounts = {
-    "/root/.npm"       : persistent('npm-cache'),
-    "/.tmux.conf"      : join(env.HOME, ".tmux.conf"),
-    "/azk/demos"       : "../demos",
-    "/azk/build"       : persistent('build-#{system.name}'),
-    "/azk/lib"         : persistent('lib-#{system.name}'),
-    "/azk/data"        : persistent('data-#{system.name}'),
-    "/azk/aptly"       : persistent('aptly-#{system.name}'),
-    "/var/lib/docker"  : persistent('docker_files-#{system.name}'),
-    "/azk/#{manifest.dir}/node_modules" : persistent('node_modules-#{system.name}'),
-    "/root/.aptly.conf": path("./src/libexec/aptly.json")
-  }
+var envs = {
+  PATH: "/azk/#{manifest.dir}/bin:/usr/local/bundle/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+  AZK_DATA_PATH: "/azk/data",
+  AZK_LIB_PATH : "/azk/lib",
+  AZK_NAMESPACE: "azk.linux",
+  AZK_PACKAGE_PATH: "/azk/build",
+  AZK_BALANCER_HOST: "linux.azk.io",
+  AZK_BALANCER_PORT: 8080,
+  LOG: "file",
+  EXTRA_SCRIPT: "/azk/#{manifest.dir}/src/libexec/init_azk",
+  VERSION: "#{azk.version}",
+};
 
-  var glob  = require('glob');
-  var itens = glob.sync("./!(lib|data|node_modules|npm-debug.log|.git|.DS_Store|.azk)", { dot: true });
+systems({
+  package: {
+    image: { docker: 'azukiapp/fpm:1.4.0' },
+    shell: "/bin/bash",
+    workdir: "/azk/#{manifest.dir}",
+    command: "azk nvm ./src/libexec/package-tools/server",
+    scalable: { default: 0 },
+    mounts: mounts,
+    envs: envs,
+    http: {
+      domains: ["#{system.name}.azk.#{azk.default_domain}"],
+    },
+    ports: {
+      http: "8080/tcp",
+    },
+  },
 
-  mounts = lodash.reduce(itens, function(mount, item) {
-    var key    = join("/azk", "#{manifest.dir}", item);
-    mount[key] = path(item);
-    return mount;
-  }, mounts);
-
-  return mounts;
-})();
-
-var agent_system = function(image, extras) {
-  extras = extras || {};
-
-  return lodash.merge({
-    image: { docker: image },
-    scale: false,
+  'dind-ubuntu': {
+    image: { docker: 'azukiapp/dind:ubuntu14' },
+    scalable: false,
     workdir: "/azk/#{manifest.dir}",
     shell: "/usr/local/bin/wrapdocker",
     mounts: mounts,
-    envs: {
-      PATH: "/azk/#{manifest.dir}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-      AZK_DATA_PATH: "/azk/data",
-      AZK_LIB_PATH : "/azk/lib",
-      AZK_NAMESPACE: "azk.linux",
-      AZK_PACKAGE_PATH: "/azk/build",
-      AZK_BALANCER_HOST: "linux.azk.io",
-      AZK_BALANCER_PORT: 8080,
-      LOG: "file",
-      EXTRA_SCRIPT: "/azk/#{manifest.dir}/src/libexec/init_azk",
-      VERSION: "#{azk.version}",
-    },
+    envs: envs,
     docker_extra: {
       HostConfig: { Privileged: true },
     }
-  }, extras);
-}
+  },
 
-var test_package_system = function(image){
-  return {
+  'dind-fedora': {
+    extends: "dind-ubuntu",
+    image: { docker: 'azukiapp/dind:fedora20' },
+  },
+
+  'pkg-ubuntu12-test': {
     depends: ["package"],
-    image: { docker: image },
+    image: { docker: 'azukiapp/dind:ubuntu12' },
     workdir: "/azk/#{manifest.dir}",
     shell: "/usr/local/bin/wrapdocker",
     mounts: {
@@ -80,55 +84,15 @@ var test_package_system = function(image){
     docker_extra: {
       HostConfig: { Privileged: true },
     }
-  }
-}
-
-systems({
-  'dind-ubuntu': agent_system('azukiapp/dind:ubuntu14'),
-  'dind-fedora': agent_system('azukiapp/dind:fedora20'),
-
-  package: agent_system('azukiapp/fpm', {
-    shell: "/bin/bash",
-    command: "azk nvm ./src/libexec/package-tools/server",
-    scalable: { default: 0 },
-    http: {
-      domains: ["#{system.name}.azk.#{azk.default_domain}"],
-    },
-    ports: {
-      http: "8080/tcp",
-    },
-  }),
-
-  'pkg-ubuntu12-test': test_package_system('azukiapp/dind:ubuntu12'),
-  'pkg-ubuntu14-test': test_package_system('azukiapp/dind:ubuntu14'),
-  'pkg-fedora-test'  : test_package_system('azukiapp/dind:fedora20'),
-
-  grunt: {
-    image: { docker: "azukiapp/node" },
-    workdir: "/azk/#{manifest.dir}",
-    mounts: {
-      "/azk/#{manifest.dir}": ".",
-    },
-    envs: {
-      PATH: "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/azk/#{manifest.dir}/node_modules/.bin"
-    }
   },
 
-  docs: {
-    image: { docker: "azukiapp/python" },
-    //provision: [
-      //'export INSTALL_DIR=/azk/<%= manifest.dir %>/vendor/python',
-      //'pip install --target=$INSTALL_DIR --install-option="--install-scripts=$INSTALL_DIR/bin" sphinx',
-    //],
-    workdir: "/azk/#{manifest.dir}",
-    mounts: {
-      "/azk/#{manifest.dir}": ".",
-    },
-    //envs: {
-      //PYTHONPATH: "/azk/<%= manifest.dir %>/vendor/python",
-      //PATH: "/bin:/sbin:/usr/bin:/usr/sbin:/azk/<%= manifest.dir %>/vendor/python/bin"
-    //}
+  'pkg-ubuntu14-test': {
+    extends: "pkg-ubuntu12-test",
+    image: { docker: 'azukiapp/dind:ubuntu14' },
+  },
+
+  'pkg-ubuntu14-test': {
+    extends: "pkg-ubuntu12-test",
+    image: { docker: 'azukiapp/dind:fedora20' },
   },
 });
-
-setDefault('docs');
