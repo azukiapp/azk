b9121d3dc2c633507fea6ce1e216af2532a7174b
Éverton Ribeiro <nuxlli@gmail.com>
2015-12-09 12:11:05 -0200

    [bug-report] Fixing cli config tests and deploy spec

--- spec/cmds/config_spec.js
+++ spec/cmds/config_spec.js
@@ -21,21 +21,21 @@ describe('Azk cli, config controller', function() {
       h.expect(code).to.equal(0);
       h.expect(options).to.have.property('config', true);
 
-      // match
-      h.expect(outputs[0]).to.match(RegExp(h.escapeRegExp('user.email')));
-
-      // match
-      h.expect(outputs[0]).to.match(RegExp(h.escapeRegExp('crash_reports.always_send')));
+      var ui_result = outputs.join("\n");
+      h.expect(ui_result).to.match(/user\.email/);
+      h.expect(ui_result).to.match(/crash_reports\.always_send/);
     });
   });
 
-  it.only("should azk config list show user.email configuration status", function() {
+  it("should azk config list show user.email configuration status", function() {
     doc_opts.argv = ['config', 'list', 'user.email'];
     var options = cli.router.cleanParams(cli.docopt(doc_opts));
     return cli.run(doc_opts, run_options).then((code) => {
       h.expect(code).to.equal(0);
       h.expect(options).to.have.property('config', true);
-      h.expect(outputs[0]).to.match(/user.email: /);
+
+      var ui_result = outputs.join("\n");
+      h.expect(ui_result).to.match(/user\.email.*:\s.*null/);
     });
   });
 });
