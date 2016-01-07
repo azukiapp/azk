03658bc09c4d2c390b650cd5e9709075386bc475
Éverton Ribeiro <nuxlli@gmail.com>
2016-01-06 22:13:29 -0200

    [Code] Removing `chai` and replacing by `azk-dev/lib/chai`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -25,6 +25,7 @@ This project adheres to [Semantic Versioning](http://semver.org/).
   * [Agent] Expanding error mensage in agent starting process;
   * [Dependency] Updating the `azk-dev` and exchanging `babel-runtime` with `babel-poliffy`.
   * [Code] Refactoring many parts of the code to suit best the modules of the system `babel> 6`;
+  * [Code] Removing `chai` and replacing by `azk-dev/lib/chai`;
 
 * Bug
   * [Pull] When pulling in verbose mode do not print several `undefined` anymore. Instead print Docker downloading bar provided by Docker API;
--- spec/spec_helpers/mocha_with_async.js
+++ spec/spec_helpers/mocha_with_async.js
@@ -1,39 +1,6 @@
-import { path } from 'azk';
 import { async } from 'azk/utils/promises';
 
-function isGeneratorFunction (fn) {
-  return typeof fn === 'function' &&
-    fn.constructor &&
-    fn.constructor.name === 'GeneratorFunction';
-}
-
 export function extend() {
-  var suffix    = path.sep + path.join('', 'mocha', 'index.js');
-  var children  = require.cache || {};
-
-  var modules = Object.keys(children).filter(function (child) {
-    return child.slice(suffix.length * -1) === suffix;
-  }).map(function (child) {
-    return children[child].exports;
-  });
-
-  modules.forEach((mocha) => {
-    var Runnable = mocha.Runnable;
-    var run = Runnable.prototype.run;
-
-    if (Runnable.__generatorsIsLoaded) {
-      return true;
-    }
-
-    Runnable.prototype.run = function (fn) {
-      if (isGeneratorFunction(this.fn)) {
-        var _fn = this.fn;
-        this.fn = () => async(this, _fn);
-      }
-
-      return run.call(this, fn);
-    };
-
-    Runnable.__generatorsIsLoaded = true;
-  });
+  let fn = (...args) => async(...args);
+  require('azk-dev/lib/generators')(fn);
 }
