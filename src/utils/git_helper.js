5cf55abc6b17074fed08b086dfa73462179eec63
Éverton Ribeiro <nuxlli@gmail.com>
2016-03-02 00:20:02 -0300

    [utils] Refactoring git helper to reduce code duplication

--- src/utils/git_helper.js
+++ src/utils/git_helper.js
@@ -1,101 +1,51 @@
 import { lazy_require, path } from 'azk';
 import { promiseResolve, promiseReject } from 'azk/utils/promises';
 import { groupFromRegex } from 'azk/utils/regex_helper';
-// matchFirstRegex, matchAllRegex, fulltrim,
 
 var lazy = lazy_require({
   spawnAsync : ['azk/utils/spawn_helper'],
 });
 
 var gitHelper = {
-  version: (scanFunction) => {
-    return lazy.spawnAsync('git', ['--version'], scanFunction)
-    .then((result) => {
-      if (result.error_code === 0) {
-        var git_version = groupFromRegex(result.message, /.*?(\d+\.\d+\.\d+)/, 1);
-        return promiseResolve(git_version);
-      } else {
-        return promiseReject(result.message);
-      }
-    });
+  version(scanFunction) {
+    let git_params = ['--version'];
+    let format = (result) => groupFromRegex(result.message, /.*?(\d+\.\d+\.\d+)/, 1);
+    return this._spawn_async(git_params, scanFunction, format);
   },
 
-  revParse: (revision, git_path, scanFunction) => {
-    return lazy.spawnAsync('git', [
-      '--git-dir',
-      git_path,
-      'rev-parse',
-      '--verify',
-      revision
-    ], scanFunction)
-    .then((result) => {
-      if (result.error_code === 0) {
-        const commit_id = result.message.substring(0, 7);
-        return promiseResolve(commit_id);
-      } else {
-        return promiseReject(result.message);
-      }
-    });
-  },
-
-  show: (revision, format, git_path, scanFunction) => {
-    return lazy.spawnAsync('git', [
-      '--git-dir',
-      git_path,
-      'show',
-      '-s',
-      '--format=' + format,
-      revision,
-    ], scanFunction)
-    .then((result) => {
-      if (result.error_code === 0) {
-        return promiseResolve(result.message);
-      } else {
-        return promiseReject(result.message);
-      }
-    });
-  },
-
-  lsRemote: (git_url, scanFunction) => {
-    return lazy.spawnAsync('git', [
-      'ls-remote',
-      git_url,
-    ], scanFunction)
-    .then((result) => {
-      if (result.error_code === 0) {
-        return promiseResolve(result.message);
-      } else {
-        return promiseReject(result.message);
-      }
-    });
-  },
-
-  clone: (git_url, git_branch_tag_commit, dest_folder, is_new_git, scanFunction) => {
-    var git_params = [
-      'clone',
-      git_url,
-      dest_folder,
-      '--recursive'
+  revParse(revision, git_path, scanFunction) {
+    let git_params = [
+      '--git-dir', git_path, 'rev-parse', '--verify', revision
     ];
+    let format = (result) => result.message.substring(0, 7);
+    return this._spawn_async(git_params, scanFunction, format);
+  },
+
+  show(revision, format, git_path, scanFunction) {
+    let git_params = [
+      '--git-dir', git_path, 'show', '-s', '--format=' + format, revision
+    ];
+    return this._spawn_async(git_params, scanFunction);
+  },
+
+  lsRemote(git_url, scanFunction) {
+    let git_params = [ 'ls-remote', git_url ];
+    return this._spawn_async(git_params, scanFunction);
+  },
+
+  clone(git_url, git_branch_tag_commit, dest_folder, is_new_git, scanFunction) {
+    var git_params = [ 'clone', git_url, dest_folder, '--recursive' ];
 
     if (is_new_git) {
-      // git_params.push('--single-branch');
       git_params.push('--branch');
       git_params.push(git_branch_tag_commit);
     }
 
-    return lazy.spawnAsync('git', git_params, scanFunction)
-    .then((result) => {
-      if (result.error_code === 0) {
-        return promiseResolve(result.message);
-      } else {
-        return promiseReject(result.message);
-      }
-    });
+    return this._spawn_async(git_params, scanFunction);
   },
 
-  pull: (git_url, git_branch_tag_commit, dest_folder, scanFunction) => {
-    return lazy.spawnAsync('git', [
+  pull(git_url, git_branch_tag_commit, dest_folder, scanFunction) {
+    let git_params = [
       '--git-dir',
       path.resolve(dest_folder, '.git'),
       '--work-tree',
@@ -103,27 +53,30 @@ var gitHelper = {
       'pull',
       git_url,
       git_branch_tag_commit
-    ], scanFunction)
-    .then((result) => {
-      if (result.error_code === 0) {
-        return promiseResolve(result.message);
-      } else {
-        return promiseReject(result.message);
-      }
-    });
+    ];
+
+    return this._spawn_async(git_params, scanFunction);
   },
 
-  checkout: (git_branch_tag_commit, dest_folder, scanFunction) => {
-    return lazy.spawnAsync('git',
-      ['-C', dest_folder, 'checkout', git_branch_tag_commit],
-      scanFunction)
-    .then((result) => {
-      if (result.error_code === 0) {
-        return promiseResolve(result.message);
-      } else {
-        return promiseReject(result.message);
-      }
-    });
+  checkout(git_branch_tag_commit, dest_folder, scanFunction) {
+    let git_params = ['-C', dest_folder, 'checkout', git_branch_tag_commit];
+    return this._spawn_async(git_params, scanFunction);
+  },
+
+  _spawn_async(git_params, scanFunction, format = null) {
+    if (!format) {
+      format = (result) => result.message;
+    }
+
+    return lazy
+      .spawnAsync('git', git_params, scanFunction)
+      .then((result) => {
+        if (result.error_code === 0) {
+          return promiseResolve(format(result));
+        } else {
+          return promiseReject(result.message);
+        }
+      });
   },
 
 };
