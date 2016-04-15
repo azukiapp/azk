febf355d506741d64f4a2a423fa875b52016e363
Felipe Arenales <fearenales@gmail.com>
2016-04-15 11:29:11 -0300

    [Package] Fixing Homebrew formula generated by 'pack.sh'

--- src/libexec/package-tools/mac/generate.sh
+++ src/libexec/package-tools/mac/generate.sh
@@ -48,15 +48,36 @@ class ${CLASS_NAME} < Formula
   url "http://${MAC_BUCKET_URL}/mac/azk_${VERSION}.tar.gz"
   version "${VERSION}"
   sha256 "${SHA256}"
+  head "https://github.com/azukiapp/azk.git"
   ${CONFLICTS}
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
 
   def install
-    prefix.install Dir['*']
-    prefix.install Dir['.nvmrc']
-    prefix.install Dir['.dependencies']
-    prefix.install Dir['.package-envs']
+    items_path = '.'
+    if build.head?
+      ENV.deparallelize
+      ENV['HOMEBREW_TEMP'] = buildpath
+      system 'make', '-e', 'package_mac'
+
+      items_path = 'package/brew/*/usr/lib/azk'
+      items = %w{ bin lib node_modules shared package.json npm-shrinkwrap.json CHANGELOG.md LICENSE README.md }
+    else
+      items = ['*']
+    end
+    items += %w{ .dependencies .nvmrc .package-envs }
+
+    items.each do |item|
+      prefix.install Dir["#{items_path}/#{item}"]
+    end
+  end
+
+  def post_install
+    rmtree './package' if build.head?
+  end
+
+  test do
+    system "azk", "version"
   end
 end
 
